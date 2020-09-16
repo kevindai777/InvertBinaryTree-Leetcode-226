2ec695eed75294993fae6f22a258b960f38b6471
Kevin Dai <kevin.dai.2451@gmail.com>
2020-09-16 03:37:50 -0400

    added java solution

---
+++ invertbinarytree.java
@@ -0,0 +1,17 @@
+//Java Solution
+
+class Solution {
+    public TreeNode invertTree(TreeNode root) {
+        if (root == null) {
+            return null;
+        }
+        
+        TreeNode left = invertTree(root.left);
+        TreeNode right = invertTree(root.right);
+        
+        root.left = right;
+        root.right = left;
+        
+        return root;
+    }
+}
